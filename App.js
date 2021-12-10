2dbddd1f77839e9ab90b6cacac6318c5f94f5609
Steven Chavarria <steven.chavarria4@gmail.com>
2021-12-09 21:30:12 -0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import api from './api/dealsService'
+import Deals from './components/deals'
+
 
 export default function App() {
+  const [deals, setDeals] = useState([]);
+
+
+  useEffect(() => {
+    (async () => {
+      const deals = await api.fetchInitialDeals();
+      //console.info(deals);
+      setDeals(deals);
+    })();
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {deals.length > 0 ?
+        <Deals />
+        : <Text styles={styles.header}>BigSale app!</Text>}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +34,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 40
   },
 });
